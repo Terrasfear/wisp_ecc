eaa1baf7fb9342d961eb5a8012799237491c633f
Casper van der Horst <Casper van der Horst>
2017-01-23 11:11:09 +0100

    ECC.c werkend

--- ECC.c
+++ ECC.c
@@ -18,20 +18,50 @@ int ipow(int base, int exp)                     /*source: http://stackoverflow.c
     return result;
 }
 
+int on_curve(int p[2], int param[5])
+{
+    if (MOD(ipow(p[1], 2), param[4]) == MOD(param[0]*ipow(p[0], 3) + param[1]*ipow(p[0], 2) + param[2]*p[0] + param[3], param[4])) return 1;
+    else return 0;
+}
+
+void print_all_points(int par[5])
+{
+    int i, j;
+    int p[2];
+    printf("----------------------\nAll integer points on curve:\n\n");
+    for(i = 0; i < par[4]; i++)
+    {
+        p[0] = i;
+        for(j = 0; j < par[4]; j++)
+        {
+            p[1] = j;
+            if (on_curve(p, par) == 1)
+            {
+                printf("(%d, %d), ", p[0], p[1]);
+            }
+        }
+        printf("\n");
+    }
+    printf("----------------------\n");
+}
+
 int* ECC_addition(int P1[], int P2[], int param[])
 {
     int m;
     static int q[2];
 
-    if(P1 == P2)
+    if(P1[0] == P2[0] && P1[1] == P2[1])
     {
+        printf("3A\n");
         m = (3 * param[0] * ipow(P1[0], 2) + 2 * param[1] * P1[0] + param[2]) * ipow(2, param[4]-2) * ipow(P1[1], (param[4]-2));
     }
     else
     {
-        m = (P2[1] -P1[1])*ipow((P2[0]-P1[0]),(param[4]-2));
+        printf("3B\n");
+        m = (P2[1] -P1[1]) *ipow((P2[0]-P1[0]),(param[4]-2));
     }
     m  = MOD(m, param[4]);
+    printf("m= %d\n", m);
 
     q[0] = - P1[0] - P2[0] - param[1] * ipow(param[0], (param[4]-2)) + ipow(m,2) * ipow(param[0], (param[4]-2));
     q[0]  = MOD(q[0], param[4]);
@@ -100,14 +130,13 @@ int* generate_key (int private_key, int start[2], int param[5])
     return P3;
 }
 
-
 int main(void)
 {
-    int start[2]={1,5};
-    int private_key=2;
+    int start[2]={1,4};
+    int private_key=5;
     /*int a, b, c, d, p;      /* y^2 = a x^3 + b x^2 + c x + d*/
                             /* p staat voor priem getal*/
-    int param[5] = {1, 0, 1, 5, 7};
+    int param[5] = {1, 0, 2, 2, 11};
     int *Q;
 
     Q = generate_key(private_key, start, param);
