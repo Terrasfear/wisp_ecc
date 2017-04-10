a97026fe811b1ea7bae49ad313f35d455bcf4394
martijnvandermarel <martijn.vander.marel@hetnet.nl>
2017-04-10 12:45:08 +0200

    Update ECC.h

--- ECC_on_wisp/ECC_multiplication/ECC.h
+++ ECC_on_wisp/ECC_multiplication/ECC.h
@@ -9,7 +9,7 @@
 #define ECC_H_
 
 uint8_t* ECC_multiplication(uint8_t P[], uint8_t n, uint8_t param[]);
-uint8_t* ECC_addition(uint8_t P1[], uint8_t P2[], uint8_t param[]);
+uint16_t* ECC_addition(uint8_t P1[], uint8_t P2[], uint8_t param[]);
 uint16_t ModInv(uint16_t a, uint16_t m);
 uint16_t gcdExtended(uint16_t a, uint16_t b, uint16_t *x, uint16_t *y);
 uint8_t ipow(uint8_t base, uint8_t exp, uint8_t m);
